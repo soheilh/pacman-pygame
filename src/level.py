2121aa69bcd6f155ffd456abceba49d251191477
soheil <soheilh105@gmail.com>
2024-07-16 15:58:01 +0330

    refactor: Optimized wall detection and adjacency checks, fixed minor issues in wall drawing
    - level.py
        - Introduced wall_letter list for wall tile types.
        - Moved dimension calculation outside nested loops for efficiency.
        - Simplified horizontal and vertical wall condition checks.
        - Ensured consistent handling of wall tile types '1' and '2' during adjacency checks.
        - Fixed minor issues in wall drawing.

--- src/level.py
+++ src/level.py
@@ -9,23 +9,25 @@ def load_level(file_path, wall_group, score_group, tile_size):
     with open(file_path, 'r') as file:
         level = [list(line.replace("\n", "")) for line in file]
 
+    # Determine the dimensions of the level
+    max_x = len(level[0]) - 1
+    max_y = len(level) - 1
+
+    wall_letter = ['1', '2']
+
     for y, row in enumerate(level):
         for x, tile in enumerate(row):
             if tile == '1' or tile == '2':
                 wall = None
                 
-                # Determine the dimensions of the level
-                max_x = len(level[0]) - 1
-                max_y = len(level) - 1
-                
                 # Set adjacency variables, handling out-of-bounds by defaulting to False
-                top = (y > 0) and (level[y - 1][x] == tile)
-                bottom = (y < max_y) and (level[y + 1][x] == tile)
-                left = (x > 0) and (level[y][x - 1] == tile)
-                right = (x < max_x) and (level[y][x + 1] == tile)
-                top_right = (y > 0 and x < max_x) and (level[y - 1][x + 1] == tile)
-                top_left = (y > 0 and x > 0) and (level[y - 1][x - 1] == tile)
-                bottom_right = (y < max_y and x < max_x) and (level[y + 1][x + 1] == tile)
+                top = (y > 0) and (level[y - 1][x] in wall_letter)
+                bottom = (y < max_y) and (level[y + 1][x] in wall_letter)
+                left = (x > 0) and (level[y][x - 1] in wall_letter)
+                right = (x < max_x) and (level[y][x + 1] in wall_letter)
+                top_right = (y > 0 and x < max_x) and (level[y - 1][x + 1] in wall_letter)
+                top_left = (y > 0 and x > 0) and (level[y - 1][x - 1] in wall_letter)
+                bottom_right = (y < max_y and x < max_x) and (level[y + 1][x + 1] in wall_letter)
                 bottom_left = (y < max_y and x > 0) and (level[y + 1][x - 1] == tile)
                 
                 # Check for border conditions and internal conditions
@@ -37,9 +39,9 @@ def load_level(file_path, wall_group, score_group, tile_size):
                     wall = Wall(x * tile_size, y * tile_size, tile_size, tile_size, f"{tile}-circle-bottomright")
                 elif (top and not bottom and not left and right) or (top and bottom and left and right and not top_right):
                     wall = Wall(x * tile_size, y * tile_size, tile_size, tile_size, f"{tile}-circle-bottomleft")
-                elif left and right:
+                elif (right and left) or ((right or left) and not (top or bottom)):
                     wall = Wall(x * tile_size, y * tile_size, tile_size, tile_size, f"{tile}-line-horizontal")
-                elif top and bottom:
+                elif (top and bottom) or ((top or bottom) and not (right or left)):
                     wall = Wall(x * tile_size, y * tile_size, tile_size, tile_size, f"{tile}-line-vertical")
 
                 if wall is None:
