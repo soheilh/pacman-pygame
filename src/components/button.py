711e2231c0b5df7bd7dc9643e21b5a2ecf788a12
soheil <soheilh105@gmail.com>
2024-07-27 17:01:49 +0330

    refactor: Update Button component with action handling
    Enhanced the Button class to support actions and handle mouse and keyboard events.

--- src/components/button.py
+++ src/components/button.py
@@ -1,9 +1,9 @@
 import pygame
 
-class Button():
-    def __init__(self, pos, text_input, font, color, hover_color, rect_hover_color):
-        self.x_pos = pos[0]
-        self.y_pos = pos[1]
+class Button:
+    def __init__(self, pos, text_input, action, font, color, hover_color, rect_hover_color):
+        self.x_pos, self.y_pos = pos
+        self.action = action
         self.font = font
         self.color = color
         self.hover_color = hover_color
@@ -16,9 +16,7 @@ class Button():
         screen.blit(self.text, self.rect)
     
     def check_for_input(self, position):
-        if self.rect.collidepoint(position):
-            return True
-        return False
+        return self.rect.collidepoint(position)
     
     def change_style(self, screen):
         pygame.draw.rect(screen, self.rect_hover_color, self.rect.inflate(20, 10))
@@ -26,3 +24,9 @@ class Button():
 
     def reset_style(self):
         self.text = self.font.render(self.text_input, True, self.color)
+
+    def event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN and self.check_for_input(event.pos):
+            return self.action
+        if event.type == pygame.KEYDOWN and event.key in [pygame.K_RIGHT, pygame.K_RETURN]:
+            return self.action
